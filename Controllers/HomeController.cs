111eb13edeed25fc8b86103cdc0bb534c26e3919
Ayush <ayushchawda123@gmail.com>
2025-06-30 17:34:46 +0530

    update the Crud operation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Entity_framework.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Entity_framework.Controllers;
 
@@ -19,11 +20,122 @@ public class HomeController : Controller
     {
         this.studentDb = studentDb;
     }
+
+    // Home page Show student List 
     public IActionResult Index()
     {
         var std = studentDb.Students_Details.ToList();
         return View(std);
     }
+
+
+    // Add the new Student 
+    public IActionResult Create()
+    {
+        
+        return View();
+    }
+
+    [HttpPost]
+    public async Task <IActionResult> Create(Student std)
+    {
+        if (ModelState.IsValid)
+        {
+            await studentDb.Students_Details.AddAsync(std);
+            await studentDb.SaveChangesAsync();
+            TempData["insert_sucess"] = "Student Add Succesfully";
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View(std);
+    }
+
+    // View the student details 
+
+    public async Task <IActionResult> Details(int? id)
+    {
+        if(id==null || studentDb.Students_Details == null)
+        {
+            return NotFound();
+        }
+        var std = await studentDb.Students_Details.FirstOrDefaultAsync(x => x.Id == id);
+        if (std == null)
+        {
+            NotFound();
+        }
+        return View(std);
+    }
+
+    // Edit the student details 
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null || studentDb.Students_Details == null)
+        {
+            return NotFound();
+        }
+        var std = await studentDb.Students_Details.FindAsync(id);
+        if (std == null)
+        {
+            NotFound();
+        }
+        return View(std);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(int? id ,Student std)
+    {
+        if (id != std.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            studentDb.Students_Details.Update(std);
+            await studentDb.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View(std);
+    }
+
+
+    // Removing the Student from the table 
+
+    public async Task<IActionResult> Delete(int? id )
+    {
+      
+
+        if (id == null || studentDb.Students_Details == null )
+        {
+            return NotFound();
+        }
+        var std = await studentDb.Students_Details.FirstOrDefaultAsync(x => x.Id == id);
+        
+        return View(std);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteStudent(int? id)
+    {
+
+        var std = await studentDb.Students_Details.FindAsync(id);
+        if (std != null)
+        {
+            studentDb.Students_Details.Remove(std);
+            await studentDb.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+        else
+        {
+            return NotFound();
+        }
+
+    }
+
+
+
 
     public IActionResult Privacy()
     {
--- Migrations/20250630111601_Student_CRUD.Designer.cs
+++ Migrations/20250630111601_Student_CRUD.Designer.cs
@@ -3,6 +3,7 @@ using Entity_framework.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Entity_framework.Migrations
 {
     [DbContext(typeof(StudentDbContext))]
-    partial class StudentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250630111601_Student_CRUD")]
+    partial class Student_CRUD
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
